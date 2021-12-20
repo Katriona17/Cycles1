1c8c41998f913e5af15f473b2cef1f0c0e063ff2
Katriona17 <krk13@mail.ru>
2021-12-20 23:54:15 +0500

    Changes added

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,13 +2,13 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-    public int calculate (int startlimit, int endlimit, int counter) {
-
+    public int calculate(int startlimit, int endlimit) {
+        int counter = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= startlimit & i * i <= endlimit) {
                 counter++;
             }
         }
-           return counter;
+        return counter;
     }
 }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -13,9 +13,58 @@ class SQRServiceTest {
         int endlimit = 200;
         int expected = 5;
 
-        int actual = sqrService.calculate (startlimit, endlimit, counter);
+        int actual = sqrService.calculate (startlimit, endlimit);
 
         assertEquals(expected, actual);
 
+    }
+    @Test
+    void shouldSquareRootOnlyOne () {
+        SQRService sqrService = new SQRService ();
+        int startlimit = 100;
+        int endlimit = 100;
+        int expected = 1;
+
+        int actual = sqrService.calculate (startlimit, endlimit);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldSquareRootZero () {
+        SQRService sqrService  = new SQRService ();
+        int startlimit = 10;
+        int endlimit = 20;
+        int expected = 0;
+
+        int actual = sqrService.calculate (startlimit, endlimit);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateIfUnderBorder () {
+        SQRService sqrService = new SQRService ();
+        int i < 10;
+        int startlimit = 100;
+        int endlimit = 200;
+        int expected = 0;
+
+        int actual = sqrService.calculate (startlimit, endlimit);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateIfOverBorder () {
+        SQRService sqrService = new SQRService ();
+        int i > 99;
+        int startlimit = 100;
+        int endlimit = 200;
+        int expected = 0;
+
+        int actual = sqrService.calculate (startlimit, endlimit);
+
+        assertEquals(expected, actual);
     }
 }
