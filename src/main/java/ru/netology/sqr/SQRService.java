238ccbefb1249d9351e6b539ce322cc850ab4d5a
Katriona17 <krk13@mail.ru>
2021-12-26 21:57:49 +0500

    Test changed

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,10 +2,10 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-    public int calculate(int startlimit, int endlimit) {
+    public int calculate(int startLimit, int endLimit) {
         int counter = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i * i >= startlimit & i * i <= endlimit) {
+            if (i * i >= startLimit & i * i <= endLimit) {
                 counter++;
             }
         }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -6,64 +6,61 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
     @Test
-
-    void shouldCounterTill200 () {
-        SQRService sqrService = new SQRService ();
-        int startlimit = 100;
-        int endlimit = 200;
+    void shouldCounterTill200() {
+        SQRService sqrService = new SQRService();
+        int startLimit = 100;
+        int endLimit = 200;
         int expected = 5;
 
-        int actual = sqrService.calculate (startlimit, endlimit);
+        int actual = sqrService.calculate(startLimit, endLimit);
 
         assertEquals(expected, actual);
-
     }
+
     @Test
-    void shouldSquareRootOnlyOne () {
-        SQRService sqrService = new SQRService ();
-        int startlimit = 100;
-        int endlimit = 100;
+    void shouldSquareRootOnlyOne() {
+        SQRService sqrService = new SQRService();
+        int startLimit = 100;
+        int endLimit = 100;
         int expected = 1;
 
-        int actual = sqrService.calculate (startlimit, endlimit);
+        int actual = sqrService.calculate(startLimit, endLimit);
 
         assertEquals(expected, actual);
     }
 
     @Test
-    void shouldSquareRootZero () {
-        SQRService sqrService  = new SQRService ();
-        int startlimit = 10;
-        int endlimit = 20;
+    void shouldSquareRootZero() {
+        SQRService sqrService = new SQRService();
+        int startLimit = 10;
+        int endLimit = 20;
         int expected = 0;
 
-        int actual = sqrService.calculate (startlimit, endlimit);
+        int actual = sqrService.calculate(startLimit, endLimit);
 
         assertEquals(expected, actual);
     }
 
     @Test
-    void shouldCalculateIfUnderBorder () {
-        SQRService sqrService = new SQRService ();
-        int i < 10;
-        int startlimit = 100;
-        int endlimit = 200;
-        int expected = 0;
+    void shouldSquareRootForUpBorder() {
+        SQRService sqrService = new SQRService();
+        int startLimit = 9801;
+        int endLimit = 9801;
+        int expected = 1;
 
-        int actual = sqrService.calculate (startlimit, endlimit);
+        int actual = sqrService.calculate(startLimit, endLimit);
 
         assertEquals(expected, actual);
     }
 
     @Test
-    void shouldCalculateIfOverBorder () {
-        SQRService sqrService = new SQRService ();
-        int i > 99;
-        int startlimit = 100;
-        int endlimit = 200;
-        int expected = 0;
+    void shouldSquareRootForDifferentBorders() {
+        SQRService sqrService = new SQRService();
+        int startLimit = 100;
+        int endLimit = 9801;
+        int expected = 90;
 
-        int actual = sqrService.calculate (startlimit, endlimit);
+        int actual = sqrService.calculate(startLimit, endLimit);
 
         assertEquals(expected, actual);
     }
